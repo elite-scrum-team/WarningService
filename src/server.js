6138baf3d287a52858d0f54aaffb4057eea7492c
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-13 15:37:40 +0100

    added morgan2

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const client = require('prom-client');
 
 const app = express();
 const port = process.env.port || 4000;
 
 app.use(bodyParser());
+app.use(morgan('dev'));
 
-app.get('/', async (req, res) {
+app.get('/', async (req, res) => {
     await res.send({
         message: 'hello world!'
     });
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`))
+
