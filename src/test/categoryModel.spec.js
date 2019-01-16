b0eebf29ca1f27675f6f46f43c85ae882f96308f
Bavac <nikolai.holt@gmail.com>
2019-01-16 09:21:44 +0100

    Added model test

---
+++ src/test/categoryModel.spec.js
@@ -0,0 +1,24 @@
+//Unit-Testing a Model’s Name and Properties
+
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists,
+} = require('sequelize-test-helpers');
+
+const categoryModel = require('../models/category');
+
+describe('src/models/category', () => {
+  const Model = categoryModel(sequelize, dataTypes);
+  const instance = new Model();
+
+  // checking if the model is the same instance as the newmodel()
+  checkModelName(Model)('category');
+
+  context('properties', () => {
+    ['id', 'name'].forEach(
+      checkPropertyExists(instance)
+    );
+  });
+});
