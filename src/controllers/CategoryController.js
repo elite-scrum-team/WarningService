9f897ce6aa1d474c0085b9cd2f2028c1e8831c8d
Bavac <nikolai.holt@gmail.com>
2019-01-18 10:36:58 +0100

    Testing warningcontroller

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -4,13 +4,7 @@ module.exports = {
     async retrive() {
         try {
             const res = await db.category.findAll();
-            if (res === undefined) {
-                return null;
-            } else if (!Array.isArray(res)) {
-                return res.dataValues;
-            } else {
-                return res.map(r => r.dataValues);
-            }
+            return res.map(r => r.dataValues);
         } catch(err) {
             console.error(err);
             throw err;
